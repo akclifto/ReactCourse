0cfbd25b40ba9202d478203a67c44d61f65bea9c
akclifto <akclifto@asu.edu>
2020-09-19 13:56:04 -0700

    adding config for js intellisense

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,22 +3,68 @@
 //JSX - Javascript XML
 //be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
 var template = React.createElement(
-  'div',
-  null,
-  ' ',
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Indecision App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
-    null,
-    'Here is some paragraph text.'
-  ),
-  '  '
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Here is some paragraph text.'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item three'
+        )
+    )
 );
+
+var myName = 'Adam';
+var age = 34;
+var loc = 'Arizona';
+
+//first challenge
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        myName.toUpperCase() + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Age: ',
+        age,
+        ' '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Location: ',
+        loc,
+        ' '
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,34 @@
 
 //JSX - Javascript XML
 //be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
-var template = <div> <h1>Indecision App</h1> <p>Here is some paragraph text.</p>  </div>;
+var template = (
+    <div> 
+        <h1>Indecision App</h1> 
+        <p>Here is some paragraph text.</p>  
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+            <li>Item three</li>
+        </ol>
+    </div>
+
+);
+
+var myName = 'Adam';
+var age = 34;
+var loc = 'Arizona';
+
+//first challenge
+var templateTwo = (
+
+    <div>
+        <h1>{myName.toUpperCase() + '!'}</h1>
+        <p> Age: {age} </p>
+        <p> Location: {loc} </p>
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
