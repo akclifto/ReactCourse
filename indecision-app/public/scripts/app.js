a07b3dd5d952bbb4ab8459a98bb9a2aacc2fe9e6
akclifto <akclifto@asu.edu>
2020-09-19 16:55:48 -0700

    challenge list and clear

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 //challenge
-var appObj = {
+var app = {
     title: 'Indecision App',
     subtitle: 'new paragraph title',
-    options: ['one', 'two']
+    options: []
 };
 
 function getOptions(options) {
@@ -24,29 +24,74 @@ function getOptions(options) {
     }
 }
 
+var onFormSubmit = function onFormSubmit(e) {
+    //previous default, full-page refreshing
+    e.preventDefault();
+
+    var newOption = e.target.elements.option.value;
+
+    if (newOption) {
+        app.options.push(newOption);
+        e.target.elements.option.value = '';
+        RenderApp();
+    }
+};
+
+//clear the list and start over
+var clearList = function clearList() {
+
+    app.options = [];
+    RenderApp();
+};
+
 //JSX - Ja74script XML
 //be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        appObj.title
-    ),
-    appObj.subtitle && appObj.subtitle != '' && React.createElement(
-        'p',
-        null,
-        ' ',
-        appObj.subtitle + '!'
-    ),
-    React.createElement(
-        'p',
-        null,
-        appObj.options && appObj.options.length > 0 ? 'Here are your options:' : 'No options available'
-    ),
-    getOptions(appObj.options)
-);
+var RenderApp = function RenderApp() {
 
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && app.subtitle != '' && React.createElement(
+            'p',
+            null,
+            ' ',
+            app.subtitle + '!'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options && app.options.length > 0 ? 'Here are your options:' : 'No options available'
+        ),
+        React.createElement(
+            'button',
+            { onClick: clearList },
+            'Remove All Items'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        getOptions(app.options),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                ' Add Option'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+//initial render
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+RenderApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,10 @@
 
 
 //challenge
-const appObj = {
+const app = {
     title: 'Indecision App',
     subtitle: 'new paragraph title',
-    options: ['one', 'two']
+    options: []
 };
 
 function getOptions(options) {
@@ -22,20 +22,53 @@ function getOptions(options) {
     }
 }
 
+const onFormSubmit = (e) => {
+    //previous default, full-page refreshing
+    e.preventDefault();
+    
+    const newOption = e.target.elements.option.value;
+
+    if(newOption){
+        app.options.push(newOption);
+        e.target.elements.option.value = '';
+        RenderApp();
+    }
+};
+
+//clear the list and start over
+const clearList = () => {
+
+    app.options = [];
+    RenderApp();
+
+};
 
 //JSX - Ja74script XML
 //be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
-const template = (
-    <div>
-        <h1>{appObj.title}</h1>
+const RenderApp = () => {
 
-        {(appObj.subtitle && appObj.subtitle != '') && <p> {appObj.subtitle + '!'}</p>}
-        <p>{appObj.options && appObj.options.length > 0 ? 'Here are your options:' : 'No options available'}</p>
-        {getOptions(appObj.options)}
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+    
+            {(app.subtitle && app.subtitle != '') && <p> {app.subtitle + '!'}</p>}
+            <p>{app.options && app.options.length > 0 ? 'Here are your options:' : 'No options available'}</p>
+            
+            <button onClick={clearList}>Remove All Items</button>
+            <p>{app.options.length}</p>
 
-    </div>
 
-);
+            {getOptions(app.options)}
+    
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button> Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
 
+//initial render
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+RenderApp();
