6607ad9ea45ede5617e8a853870be5e626854257
akclifto <akclifto@asu.edu>
2020-09-19 15:49:05 -0700

    update app.js

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,46 +1,91 @@
 'use strict';
 
-//args object - no longer bound with arrow functions
-
-
-var add = function add(a, b) {
-    // console.log(arguments)  //no access to arguments with arrow functs 
-    return a + b;
+//challenge
+var appObj = {
+    title: 'Indecision App',
+    subtitle: 'new paragraph title',
+    options: ['one', 'two']
 };
 
-console.log(add(55, 1, 50005));
+function getOptions(options) {
 
-// this keyword - no longer bound
+    if (options) {
+        return React.createElement(
+            'ol',
+            null,
+            options.map(function (opt) {
+                return React.createElement(
+                    'li',
+                    { key: opt },
+                    ' ' + opt
+                );
+            })
+        );
+    }
+}
+
+//JSX - Ja74script XML
+//be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appObj.title.toUpperCase()
+    ),
+    appObj.subtitle && appObj.subtitle != '' && React.createElement(
+        'p',
+        null,
+        ' ',
+        appObj.subtitle + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        appObj.options && appObj.options.length > 0 ? 'Here are your options:' : 'No options available'
+    ),
+    getOptions(appObj.options)
+);
+
 var user = {
-    name: 'Adam',
-    cities: ['Tucson', 'Phoenix', 'Mesa'],
-    //this accessible here with function bound to obj property (printplacedLived)
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-
-        //this method throws error for anon function, this gets set to undef.
-        // can fix by using arrow funct
-        // this.cities.forEach((city) => {
-        //     console.log(this.name + ' has lived in ' + city);
-        // });
-    }
+    name: 'akclifto',
+    age: 34,
+    loc: 'Arizona'
 };
-console.log(user.printPlacesLived());
 
-//challenge 
-var multiplier = {
-    numbers: [1, 2, 3, 5, 8],
-    multiBy: 2,
-    multiply: function multiply() {
-        var _this2 = this;
+function getLocation(loc) {
 
-        return this.numbers.map(function (num) {
-            return num * _this2.multiBy;
-        });
+    if (loc) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            loc
+        );
     }
-};
-console.log(multiplier.multiply());
+}
+
+//first challenge
+//use of ternary, logical AND and function call
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anon'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        ' Age: ',
+        user.age,
+        ' '
+    ),
+    getLocation(user.loc)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
