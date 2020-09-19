3e7cf03e04a051dde4db24fd242b3e826f464222
akclifto <akclifto@asu.edu>
2020-09-19 12:37:04 -0700

    set up babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 //JSX - Javascript XML
-// var template = <p id="someid">This is JSX from app.js</p>;
-var template = /*#__PURE__*/React.createElement("h1", {
-    id: "someid"
-  }, "This is JSX from app.js");
+var template = React.createElement(
+  'h1',
+  { id: 'someid' },
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
