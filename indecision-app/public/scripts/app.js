162c801e7626f0995b706bdca981575db7d9cbdf
akclifto <akclifto@asu.edu>
2020-09-19 20:14:24 -0700

    classes react, like java

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,108 +1,120 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 //challenge
-var app = {
-    title: 'Indecision App',
-    subtitle: 'What do you want to do?',
-    options: []
-};
+// setup constructor to take name and AudioProcessingEvent.
 
-//list out the option in the array, list form.
-function getOptions(options) {
 
-    if (options) {
-        return options.map(function (opt) {
-            return React.createElement(
-                'li',
-                { key: opt },
-                ' ',
-                opt,
-                ' '
-            );
-        });
+// person class
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anon';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
     }
-}
 
-var onFormSubmit = function onFormSubmit(e) {
-    //previous default, full-page refreshing
-    e.preventDefault();
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            // return  'Hi, I am ' + this.name + ' !';
+            return 'Hi, I am ' + this.name + '. ';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name.split(' ')[0] + ' is ' + this.age + ' years old';
+        }
+    }]);
 
-    var newOption = e.target.elements.option.value;
+    return Person;
+}();
 
-    if (newOption) {
-        app.options.push(newOption);
-        e.target.elements.option.value = '';
-        RenderApp();
+var Student = function (_Person) {
+    _inherits(Student, _Person);
+
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
+
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
     }
-};
 
-//clear the list and start over
-var clearList = function clearList() {
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var descr = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
 
-    app.options = [];
-    RenderApp();
-};
+            if (this.hasMajor()) {
+                descr += ', and is majoring in ' + this.major + '.';
+            } else {
+                descr += ', and major is undecided. ';
+            }
+            return descr;
+        }
+    }]);
 
-var makeDecision = function makeDecision() {
-    var rand = Math.floor(Math.random() * app.options.length);
-    var selected = app.options[rand];
-    alert(selected);
-};
+    return Student;
+}(Person);
 
-//JSX - Ja74script XML
-//be sure to wrap expression in div if want adjacent elements, there can be only 1 root element
-var RenderApp = function RenderApp() {
+//challenge: Traveller extends the person class
 
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        app.subtitle && app.subtitle != '' && React.createElement(
-            'h3',
-            null,
-            ' ',
-            app.subtitle
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.options && app.options.length > 0 ? 'Here are your options:' : 'No options available'
-        ),
-        React.createElement(
-            'button',
-            { disabled: app.options.length == 0, onClick: makeDecision },
-            'What Should I Do'
-        ),
-        React.createElement(
-            'button',
-            { onClick: clearList },
-            'Remove All Items'
-        ),
-        React.createElement(
-            'ol',
-            null,
-            ' ',
-            getOptions(app.options),
-            ' '
-        ),
-        React.createElement(
-            'form',
-            { onSubmit: onFormSubmit },
-            React.createElement('input', { type: 'text', name: 'option' }),
-            React.createElement(
-                'button',
-                null,
-                ' Add Option'
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
 
-//initial render
-var appRoot = document.getElementById('app');
-RenderApp();
+var Traveller = function (_Person2) {
+    _inherits(Traveller, _Person2);
+
+    function Traveller(name, age, homeLoc) {
+        _classCallCheck(this, Traveller);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveller.__proto__ || Object.getPrototypeOf(Traveller)).call(this, name, age));
+
+        _this2.homeLoc = homeLoc;
+        return _this2;
+    }
+
+    _createClass(Traveller, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+
+            var greet = _get(Traveller.prototype.__proto__ || Object.getPrototypeOf(Traveller.prototype), 'getGreeting', this).call(this);
+            if (this.homeLoc) {
+                greet += 'I am visiting from ' + this.homeLoc + '!';
+            }
+            return greet;
+        }
+    }]);
+
+    return Traveller;
+}(Person);
+
+var me = new Student('Adam Clifton', 34, 'Software Engineering');
+var him = new Student('Jim');
+var trav = new Traveller('John Baptist', 42, 'Levant');
+var other = new Traveller('Someone else');
+console.log(trav.getGreeting());
+console.log(other.getGreeting());
+
+// console.log(me.getDescription());
+// console.log(him.getDescription());
+
+// console.log(me.getGreeting(), him.getGreeting());
+// console.log(me.getGreeting(), me.getAge());
---
+++ indecision-app/src/playground/es6-classes1.js
@@ -0,0 +1,79 @@
+//challenge
+// setup constructor to take name and AudioProcessingEvent.
+
+
+// person class
+class Person {
+
+    constructor(name = 'Anon', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        // return  'Hi, I am ' + this.name + ' !';
+        return `Hi, I am ${this.name}. `;
+    }
+    
+    getDescription() {
+        return `${this.name.split(' ')[0]} is ${this.age} years old`;
+    }
+}
+
+class Student extends Person {
+
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let descr = super.getDescription();
+
+        if(this.hasMajor()){
+            descr += `, and is majoring in ${this.major}.`;
+        } else {
+            descr += ', and major is undecided. ';
+        }
+        return descr;
+
+    }
+
+}
+
+//challenge: Traveller extends the person class
+class Traveller extends Person {
+
+    constructor(name, age, homeLoc) {
+        super(name, age);
+        this.homeLoc = homeLoc;
+    }
+
+    getGreeting() {
+        
+        let greet = super.getGreeting();
+        if(this.homeLoc) {
+            greet += `I am visiting from ${this.homeLoc}!`
+        } 
+        return greet;
+
+
+    }
+}
+
+const me = new Student('Adam Clifton', 34, 'Software Engineering');
+const him = new Student('Jim');
+const trav = new Traveller('John Baptist', 42, 'Levant');
+const other = new Traveller('Someone else');
+console.log(trav.getGreeting());
+console.log(other.getGreeting());
+
+// console.log(me.getDescription());
+// console.log(him.getDescription());
+
+// console.log(me.getGreeting(), him.getGreeting());
+// console.log(me.getGreeting(), me.getAge());
